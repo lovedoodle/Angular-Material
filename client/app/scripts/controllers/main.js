0b17ad120b5a678488ffde4caea8123271265ff5
Jing Wait <jingzhaowait@gmail.com>
2016-05-10 10:54:01 -0700

    replace hover state with click even on touch screen

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -9,10 +9,34 @@
  */
 angular.module('clientApp')
   .controller('MainCtrl', function ($scope) {
-    var $scope.toolBar = "false";
-    var $scope.info = "false";
-    $scope.showToolBar() = function() {
-      $scope.info = !$scope.info;
+    function is_touch_device() {
+     return (('ontouchstart' in window)
+          || (navigator.MaxTouchPoints > 0)
+          || (navigator.msMaxTouchPoints > 0));
+    };
+
+    console.log(is_touch_device());
+
+  //   if (!is_touch_device()) {
+  //    var touchOnlyElements=document.getElementByClassName('touchOnly');
+  //    for(i=0; i<touchOnlyElements.length; i++) {
+  //      var touchOnlyElement = touchOnlyElements(i);
+  //      console.log(touchOnlyElement);
+  //      touchOnlyElement.style.display='none';
+  //    }
+   //
+  //   //  .style.display='none';
+  //  };
+
+
+    $scope.infoSign = false;
+    $scope.toolBar = false;
+    $scope.showToolBar = function() {
+      $scope.infoSign = !$scope.infoSign;
+      $scope.toolBar = !$scope.toolBar;
+    };
+    $scope.hideToolBar = function() {
+      $scope.infoSign = !$scope.infoSign;
       $scope.toolBar = !$scope.toolBar;
     }
 
