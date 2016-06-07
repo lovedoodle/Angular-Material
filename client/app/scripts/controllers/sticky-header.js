e7880b86c2da200225324e18e8972cb5a04dedc3
Jing Wait <jingzhaowait@gmail.com>
2016-06-06 23:01:14 -0700

    refactor slide show and add automatic backgrond images fadein

--- client/app/scripts/controllers/sticky-header.js
+++ client/app/scripts/controllers/sticky-header.js
@@ -9,21 +9,29 @@
  */
 angular.module('clientApp')
   .controller('StickyHeaderCtrl', function ($scope, $interval) {
-    // setInterval(function() {
-    //   console.log(new Date(), "interval");
-    //   $scope.test = "A";
-    // },1000);
-    //
-    // $scope.myButtonClick
-
-    // $scope.headerVisible = false;
+    $scope.gallery = [{url: "../../images/tent.jpg"},{url: "../../images/camping-coffee.jpg"}, {url: "../../images/fly-fishing.jpg"}, {url: "../../images/rowing.jpg"}]
     $interval(function() {
       if(window.pageYOffset >= 70) {
         jQuery("nav").addClass("visibleHeader");
       }else {
         jQuery("nav").removeClass("visibleHeader");
       }
+      // console.log(  $("#mainGallery").css("background-image"));
+      // console.log($scope.gallery[2]);
     }, 100);
+    // console.log($("#mainGallery").length, "url('"+$scope.gallery[2].url+"')");
+    $scope.currentIndex = 0;
+    $scope.currentImage = $scope.gallery[$scope.currentIndex];
+    $("#mainGallery").css("background-image", "url('"+$scope.currentImage.url+"')");
+    // debugger;
+    $interval(function () {
+      $scope.currentImage = $scope.gallery[$scope.currentIndex];
+      $scope.currentIndex < $scope.gallery.length - 1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
+
+      $("#mainGallery").css("background-image", "url('"+$scope.currentImage.url+"')");
+
+    }, 3000);
+
 
 
   });
--- client/app/scripts/directives/slideshow.js
+++ client/app/scripts/directives/slideshow.js
@@ -18,22 +18,16 @@ angular.module('clientApp')
       link: function(scope, elem, attrs) {
         console.log("testing", arguments);
         scope.currentIndex = 0; // Initially the index is at the first image
+        scope.currentImage = scope.images[scope.currentIndex];
         scope.next = function() {
-
           scope.currentIndex < scope.images.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
-
+          scope.currentImage = scope.images[scope.currentIndex];
         };
 
         scope.prev = function() {
           scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.images.length - 1;
+          scope.currentImage = scope.images[scope.currentIndex];
         };
-        scope.$watch('currentIndex', function() {
-        scope.images.forEach(function(image) {
-          image.visible = false; // make every image invisible
-        });
-
-        scope.images[scope.currentIndex].visible = true; // make the current image visible
-      });
       }
     };
   });
